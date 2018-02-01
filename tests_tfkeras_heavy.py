6ccf63f66945beb4f67984a409df8258ea799fb1
braysia <kudo@DN0a22b478.SUNet>
2018-01-31 18:48:12 -0800

    patching consume less memory

--- tests_tfkeras_heavy.py
+++ tests_tfkeras_heavy.py
@@ -1,38 +1,36 @@
+from __future__ import division
+import os
 import numpy as np
 from tensorflow.contrib.keras.python.keras.datasets import mnist
-
 from tensorflow.contrib.keras.python.keras.models import Sequential, Model
-from tensorflow.contrib.keras.python.keras.layers import Dense, Dropout, Activation, Flatten
-from tensorflow.contrib.keras.python.keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
-from tensorflow.contrib.keras.python.keras.layers import Input, merge
+from tensorflow.contrib.keras.python.keras.layers import Dense, Flatten
+from tensorflow.contrib.keras.python.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.contrib.keras.python.keras.layers.normalization import BatchNormalization
-from tensorflow.contrib.keras.python.keras.layers.merge import add
-from tensorflow.contrib.keras import optimizers
-from tensorflow.contrib.keras import callbacks
-from tensorflow.contrib.keras import metrics
-
+from tensorflow.contrib.keras import optimizers, callbacks
 from tensorflow.contrib.keras import preprocessing
 from scipy.ndimage import imread
-from sklearn.feature_extraction.image import extract_patches_2d
+from patches import extract_patches
 
-x_train = imread('tests/nuc0/nuclear.png')
-y_train = np.zeros(x_train.shape)
+
+
+x = imread('tests/nuc0/nuclear.png')
+y = np.zeros(x.shape)
 y0 = imread('tests/nuc0/feature_0.png').astype(bool)
 y1 = imread('tests/nuc0/feature_1.png').astype(bool)
-y_train[y0] = 1
-y_train[y1] = 2
-
-img_stack = np.dstack((x_train, y_train))
+y[y0] = 1
+y[y1] = 2
 
 ph, pw = 61, 61
-nsamples = 100000
+assert np.bool(ph & 0x1) and np.bool(pw & 0x1)  # check if odd
+nsamples = 1000
 frac_test = 0.1
 
-patch_stack = extract_patches_2d(img_stack, patch_size=[ph, pw], max_patches=nsamples)
-x_train = patch_stack[:-int(nsamples * frac_test), :, :, 0].astype(np.float32)
-x_test = patch_stack[-int(nsamples * frac_test):, :, :, 0].astype(np.float32)
-y_train = patch_stack[:-int(nsamples * frac_test), int(np.floor(ph/2)), int(np.floor(pw/2)), -1].astype(np.int8)
-y_test = patch_stack[-int(nsamples * frac_test):, int(np.floor(ph/2)), int(np.floor(pw/2)), -1].astype(np.int8)
+x_all, y_all = extract_patches(nsamples, x, y, ph, pw)
+
+x_train = x_all[:-int(nsamples * frac_test), :, :]
+x_test = x_all[-int(nsamples * frac_test):, :, :]
+y_train = y_all[:-int(nsamples * frac_test)]
+y_test = y_all[-int(nsamples * frac_test):]
 x_train = np.expand_dims(x_train, -1)
 x_test = np.expand_dims(x_test, -1)
 
@@ -55,7 +53,6 @@ model = Sequential([
 
 model.summary()
 
-# opt = optimizers.Adam()
 opt = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(optimizer=opt,
               loss='sparse_categorical_crossentropy',
@@ -79,12 +76,8 @@ rec = dict(acc=history.history['acc'], val_acc=history.history['val_acc'],
 np.savez('output.npz', **rec)
 
 
-import os
-print('save the architecture of a model')
 json_string = model.to_json()
-open(os.path.join('output', 'cnn_model.json'), 'w').write(json_string)
+open(os.path.join(output, 'cnn_model.json'), 'w').write(json_string)
 yaml_string = model.to_yaml()
-open(os.path.join('output', 'cnn_model.yaml'), 'w').write(yaml_string)
-print('save weights')
-model.save_weights(os.path.join('output', 'cnn_model_weights.hdf5'))
-# model.to
+open(os.path.join(output, 'cnn_model.yaml'), 'w').write(yaml_string)
+model.save_weights(os.path.join(output, 'cnn_model_weights.hdf5'))
