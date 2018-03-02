b54c70f763ef13d29745a2f3e73778afa2fb2ffd
braysia <kudo@DN0a22b47f.SUNet>
2018-03-01 17:46:18 -0800

    running working properly for the first time

--- _dc_custom_layers.py
+++ _dc_custom_layers.py
@@ -1,51 +1,3 @@
-"""
-dc_custom_layers.py
-
-Custom layers for convolutional neural networks
-
-@author: David Van Valen
-"""
-
-"""
-Import python packages
-"""
-
-import numpy as np
-from numpy import array
-import matplotlib
-import matplotlib.pyplot as plt
-import shelve
-from contextlib import closing
-
-import os
-import glob
-import re
-import numpy as np
-import fnmatch
-import tifffile as tiff
-from numpy.fft import fft2, ifft2, fftshift
-from skimage.io import imread
-from scipy import ndimage
-import threading
-import scipy.ndimage as ndi
-from scipy import linalg
-import re
-import random
-import itertools
-import h5py
-import datetime
-
-from skimage.measure import label, regionprops
-from skimage.segmentation import clear_border
-from scipy.ndimage.morphology import binary_fill_holes
-from skimage import morphology as morph
-from numpy.fft import fft2, ifft2, fftshift
-from skimage.io import imread
-from skimage.filters import threshold_otsu
-import skimage as sk
-from sklearn.utils.linear_assignment_ import linear_assignment
-from sklearn.utils import class_weight
-
 import tensorflow as tf
 from tensorflow.contrib.keras.python.keras import backend as K
 from tensorflow.contrib.keras.python.keras.layers import Layer, InputSpec, Input, Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D, AvgPool2D, Concatenate
@@ -60,36 +12,21 @@ from tensorflow.contrib.keras.python.keras.utils import conv_utils
 Custom layers
 """
 class dilated_MaxPool2D(Layer):
-	def __init__(self, pool_size=(2, 2), strides=None, dilation_rate = 1, padding='valid',
-				data_format=None, **kwargs):
+	def __init__(self, pool_size=(2, 2), strides=None, dilation_rate=1, padding='valid',
+				 data_format=None, **kwargs):
 		super(dilated_MaxPool2D, self).__init__(**kwargs)
 		data_format = conv_utils.normalize_data_format(data_format)
 		if dilation_rate != 1:
-			strides = (1,1)
+			strides = (1, 1)
 		elif strides is None:
-			strides = (1,1)
+			strides = (1, 1)
 		self.pool_size = conv_utils.normalize_tuple(pool_size, 2, 'pool_size')
 		self.strides = conv_utils.normalize_tuple(strides, 2, 'strides')
 		self.dilation_rate = dilation_rate
 		self.padding = conv_utils.normalize_padding(padding)
-		self.data_format = conv_utils.normalize_data_format(data_format)
+		self.data_format = 'channels_last'
+		# self.data_format = conv_utils.normalize_data_format(data_format)
 		self.input_spec = InputSpec(ndim=4)
-
-	def compute_output_shape(self, input_shape):
-		if self.data_format == 'channels_first':
-			rows = input_shape[2]
-			cols = input_shape[3]
-		elif self.data_format == 'channels_last':
-			rows = input_shape[1]
-			cols = input_shape[2]
-
-		rows = conv_utils.conv_output_length(rows, pool_size[0], padding = self.padding, stride = self.strides[0], dilation = dilation_rate)
-		cols = conv_utils.conv_output_length(cols, pool_size[1], padding = self.padding, stride = self.strides[1], dilation = dilation_rate)
-		
-		if self.data_format == 'channels_first':
-			return (input_shape[0], input_shape[1], rows, cols)
-		elif self.data_format == 'channels_last':
-			return (input_shape[0], rows, cols, input_shape[3])
 
 	def  _pooling_function(self, inputs, pool_size, dilation_rate, strides, padding, data_format='channels_first'):
 		backend = K.backend()
@@ -100,9 +37,8 @@ class dilated_MaxPool2D(Layer):
 
 		if data_format == 'channels_first':
 			df = 'NCHW'
-		elif data_format == 'channel_last':
+		elif data_format == 'channels_last':
 			df = 'NHWC'
-		df = 'NHWC'
 
 		if self.padding == "valid":
 			padding_input = "VALID"
